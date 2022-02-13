5539e95d97cfd90572b3238da6ea3d72b94fba02
f-teruhisa <teru_fukumoto@outlook.jp>
2022-02-13 15:07:47 +0900

    complete 6 Define S3 module

--- chapter_after_four/iam_role/main.tf
+++ chapter_after_four/iam_role/main.tf
@@ -3,7 +3,7 @@ variable "policy" {}
 variable "identifier" {}
 
 resource "aws_iam_role" "default" {
-  name = var.name
+  name               = var.name
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
@@ -19,13 +19,13 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_policy" "default" {
-  name = var.name
+  name   = var.name
   policy = var.policy
 }
 
 resource "aws_iam_role_policy_attachment" "default" {
-  policy_arn = aws_iam_role.default.arn
   role       = aws_iam_role.default.name
+  policy_arn = aws_iam_policy.default.arn
 }
 
 output "iam_role_arn" {
